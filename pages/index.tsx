9336a6b32f223d276df3572faf7e8aec01441dcf
Dylan Tarlowe <dylantarlowe>
2023-01-28 18:59:52 -0500

    apos my booty

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Banner/>
       <Preferences />
       <div className='sm:px-20 px-6 pt-6'>
-        <button className='bg-horizon text-white sm:w-1/2 w-full text-lg py-2 rounded-lg'>Let's get traveling</button>
+        <button className='bg-horizon text-white sm:w-1/2 w-full text-lg py-2 rounded-lg'>Let&apos;s get traveling</button>
       </div>
       <Image className='w-1/2 invisible sm:visible top-0 right-0 fixed z-10' src={bannerImage} alt="bannerImage" />
     </div>
